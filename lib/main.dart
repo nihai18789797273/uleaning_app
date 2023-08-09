83594819d444cc712e04f180a501ef6521efe0d4
NH-Studio <nihai1998@163.com>
2023-08-09 17:43:52 +0800

    初识provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +16,29 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('测试程序'),
+      home: const MyHome(),
+    );
+  }
+}
+
+final appCount = StateProvider<int>((ref) {
+  return 1;
+});
+
+class MyHome extends ConsumerWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int count = ref.watch(appCount);
+
+    return Scaffold(
+      body: Center(
+        child: Text(count.toString()),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        ref.read(appCount.notifier).state++;
+      }),
     );
   }
 }
